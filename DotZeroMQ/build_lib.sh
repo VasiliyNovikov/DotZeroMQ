1ef12aeb52150f3fe4d268611b895a0470ecc35b
Vasiliy Novikov <vasiliy.novikov@outlook.com>
2020-05-11 02:00:19 +0300

    libzmq 4.3.2 + native lib packaging fix (#5)

--- DotZeroMQ/build_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-wget https://github.com/zeromq/libzmq/releases/download/v4.2.5/zeromq-4.2.5.tar.gz
-tar xf zeromq-4.2.5.tar.gz -C ./
-mv zeromq-4.2.5 zeromq
-cd zeromq/
-./configure
-make
